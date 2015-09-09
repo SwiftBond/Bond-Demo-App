ff1d053b1ceb5599491603dc54448805ded9a5f9
yana_3 <yanamura3@gmail.com>
2015-09-09 17:46:37 +0900

    add dispatch_async(main) to ListViewModel

--- BondDemo/ListViewModel.swift
+++ BondDemo/ListViewModel.swift
@@ -28,34 +28,36 @@ class ListViewModel {
       // fetch JSON of repositories, starting from last repo we have
       if let data = NSData(contentsOfURL: NSURL(string: self.apiURL + "?since=\(self.since)")!) {
         if let jsonResult = (try? NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0))) as? NSArray {
-          // parse JSON
-          var newRepos: [ListCellViewModel] = []
+          dispatch_async(dispatch_get_main_queue()) {
+            // parse JSON
+            var newRepos: [ListCellViewModel] = []
 
-          for repo in jsonResult {
-            if let repo = repo as? NSDictionary {
-              let id = (repo["id"] as? Int) ?? 0
-              let name = (repo["name"] as? String) ?? ""
+            for repo in jsonResult {
+              if let repo = repo as? NSDictionary {
+                let id = (repo["id"] as? Int) ?? 0
+                let name = (repo["name"] as? String) ?? ""
 
-              var username: String = ""
-              var photoURL: String? = nil
+                var username: String = ""
+                var photoURL: String? = nil
 
-              if let owner = repo["owner"] as? NSDictionary {
-                username = (owner["login"] as? String) ?? ""
-                photoURL = owner["avatar_url"] as? String
+                if let owner = repo["owner"] as? NSDictionary {
+                  username = (owner["login"] as? String) ?? ""
+                  photoURL = owner["avatar_url"] as? String
+                }
+
+                self.since = max(self.since, id)
+
+                let cellViewModel = ListCellViewModel(name: name, username: username, photoUrl: photoURL)
+                newRepos.append(cellViewModel)
               }
-
-              self.since = max(self.since, id)
-
-              let cellViewModel = ListCellViewModel(name: name, username: username, photoUrl: photoURL)
-              newRepos.append(cellViewModel)
             }
+
+            // update view model by adding new repos to the begining of the array
+            // this will automatically trigger table view updates with new repos
+            self.repositoryCellViewModels.insertContentsOf(newRepos.reverse(), atIndex: 0)
+
+            completion()
           }
-
-          // update view model by adding new repos to the begining of the array
-          // this will automatically trigger table view updates with new repos
-          self.repositoryCellViewModels.insertContentsOf(newRepos.reverse(), atIndex: 0)
-
-          completion()
         }
       }
     }
